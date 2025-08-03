c6dc33c1d835274d8ac6b650e362466a59115c40
Evan Langford <langfordevana@gmail.com>
2025-08-03 12:11:01 -0500

    Finish 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    const arr = arguments[0];
+    const itemsToRemove = Array.prototype.slice.call(arguments, 1);
+
+    return arr.filter((item) => !itemsToRemove.includes(item));
 };
 
 // Do not edit below this line
