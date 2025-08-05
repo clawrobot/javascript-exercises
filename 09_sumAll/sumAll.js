16e2f06bda32956b8c60d8bed2ba915855969d11
Evan Langford <langfordevana@gmail.com>
2025-08-05 04:44:22 -0500

    Finish 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    ans = 0;
+    for (let i = num1; i <= num2; i++) {
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
