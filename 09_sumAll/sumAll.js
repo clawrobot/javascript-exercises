41860af39b81c9ef89da61b668986ff2fb83d5bc
Evan Langford <langfordevana@gmail.com>
2025-08-05 09:36:42 -0500

    Correct 09 with more tests applied

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,9 +1,26 @@
 const sumAll = function (num1, num2) {
-    ans = 0;
-    for (let i = num1; i <= num2; i++) {
-        ans += i;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
     }
-    return ans;
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    if (Math.floor(num1) !== num1 || Math.floor(num2) !== num2) {
+        return 'ERROR';
+    }
+    ans = 0;
+    if (num1 <= num2) {
+        for (let i = num1; i <= num2; i++) {
+            ans += i;
+        }
+        return ans;
+    }
+    else {
+        for (let i = num2; i <= num1; i++) {
+            ans += i;
+        }
+        return ans;
+    }
 };
 
 // Do not edit below this line
