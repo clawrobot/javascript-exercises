b7d28b33a15952d61f3fd99f2cdd4d1674a1c3a6
Evan Langford <langfordevana@gmail.com>
2025-08-12 21:24:09 -0500

    Finish 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length < 1) {
+    return 0;
+  }
+  return arr.reduce((acc, curr) => acc + curr);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  let ans = x;
+  for (let i = 0; i < y - 1; i++) {
+    ans = ans * x;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x === 0) {
+    return 1;
+  }
+  let ans = x;
+  for (let i = x - 1; i > 0; i--) {
+    ans = ans * i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
