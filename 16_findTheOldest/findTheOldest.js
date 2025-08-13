5f9643ed07ebd320a5d789455808c7df8e00732b
Evan Langford <langfordevana@gmail.com>
2025-08-12 23:21:21 -0500

    Finish 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function (ppl) {
+    let oldest = ppl.sort((a, b) => {
+        let bAge = 0;
+        let aAge = 0;
+        if ("yearOfDeath" in a && "yearOfDeath" in b) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        else if ("yearOfDeath" in a) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+            bAge = 2025 - b.yearOfBirth;
+        }
+        else if ("yearOfDeath" in b) {
+            aAge = 2025 - a.yearOfBirth;
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        else {
+            aAge = 2025 - a.yearOfBirth;
+            bAge = 2025 - b.yearOfBirth;
+        }
 
+        return bAge - aAge;
+    })
+    return oldest[0];
 };
 
 // Do not edit below this line
