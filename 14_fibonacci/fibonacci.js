ac7a640b1af23a308fdd7291f6125d61f8dfc523
Evan Langford <langfordevana@gmail.com>
2025-08-12 22:17:57 -0500

    Finish 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    num = Number(num);
+    if (num < 0) return 'OOPS';
+    function fib(num) {
+        if (num === 0) return 0;
+        if (num === 1) return 1;
+        return (fib(num - 1) + fib(num - 2));
+    }
+    return fib(num);
 };
 
 // Do not edit below this line
