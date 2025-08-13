66420f23776209af9f2c60e48565d7e585d3a730
Evan Langford <langfordevana@gmail.com>
2025-08-12 21:44:48 -0500

    Finish 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
 
+const palindromes = function (words) {
+    const cleaned = words.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reversed = cleaned.split("").reverse().join("");
+    return cleaned === reversed;
 };
 
 // Do not edit below this line
